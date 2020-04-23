085e37c74b43466be1280fab785b4375050702be
Viktor Andersson <v.andersson@me.com>
2020-04-23 21:43:42 +0200

    Add FSharp console application

---
+++ AppFSharp/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open MyLibrary.Types
+
+[<EntryPoint>]
+let main argv =
+    let myVar = MyType ""
+    printfn "%i" (MyTypeExtensions.ValueLength myVar) // this works
+    myVar.ValueLength() // this does not work
+    0 // return an integer exit code
